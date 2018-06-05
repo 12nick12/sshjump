e77124fdd9314b526ba5883cf37b4d794a96d053
12nick12 <nicholasleffler@gmail.com>
2018-06-05 15:34:13 -0400

    Delete start.sh

--- client/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-remoteport=$(sqlite3 /opt/sshjump/sshjump.db 'select port from sshjump')
-localport=22333
-
-autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $remoteport:localhost:$localport root@remotehost.com -p 22333 -i /opt/sshjump/id_rsa
