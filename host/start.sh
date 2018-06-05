c847f44b53e852fd6d773d4b67ce7137e7c2208b
12nick12 <nicholasleffler@gmail.com>
2018-06-05 17:14:12 -0400

    Update start.sh

--- host/start.sh
+++ host/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+servername=site.site.com
+username=root
+serverport=22
 dir=/opt/sshjump
 remoteport=$(sqlite3 $dir/sshjump.db 'select port from sshjump')
-localport=22333
+localport=22
 
-autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $remoteport:localhost:$localport root@remotehost.com -p 22333 -i $dir/id_rsa
+autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $remoteport:localhost:$localport $username@$servername -p $serverport -i $dir/id_rsa
