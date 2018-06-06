2af46c4b947a5dbc669dc1130443cfa22015d944
12nick12 <nicholasleffler@gmail.com>
2018-06-06 06:53:35 -0400

    Update start_systemd.sh

--- host/start_systemd.sh
+++ host/start_systemd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 dir=/opt/sshjump
-serverhost=user@test.test.com
+servername=user@test.test.com
+usersname=sshjump
 serviceport=22
 remoteport=$(sqlite3 $dir/sshjump.db 'select port from sshjump')
 localport=22
 
 until ping -c1 google.com &>/dev/null; do :; done
-autossh -N -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $remoteport:localhost:$localport $server -p $serverport -i $dir/id_rsa
+autossh -N -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $remoteport:localhost:$localport $username@$servername -p $serverport -i $dir/id_rsa
